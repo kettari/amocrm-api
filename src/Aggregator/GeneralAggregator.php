922180825b668bb4383b55ded5da56a8bfeef53d
ifedorchuk <bkmz92@gmail.com>
2018-02-02 14:06:47 +0300

    General generator fixes and refactor

--- src/Aggregator/GeneralAggregator.php
+++ src/Aggregator/GeneralAggregator.php
@@ -18,7 +18,8 @@ use AmoCrm\Client\Request;
 use ArrayObject;
 use Monolog\Logger;
 
-abstract class GeneralAggregator extends ArrayObject {
+abstract class GeneralAggregator extends ArrayObject
+{
 
   /**
    * Monolog logger
@@ -44,7 +45,11 @@ abstract class GeneralAggregator extends ArrayObject {
    * @param \AmoCrm\Client\CustomField\FieldConfig $field_config
    * @param \AmoCrm\Client\Request $request
    */
-  public function __construct(Logger $logger, FieldConfig $field_config, Request $request) {
+  public function __construct(
+    Logger $logger,
+    FieldConfig $field_config,
+    Request $request
+  ) {
     $this->logger = $logger;
     $this->field_config = $field_config;
     $this->request = $request;
@@ -53,7 +58,8 @@ abstract class GeneralAggregator extends ArrayObject {
   /**
    * Clear items
    */
-  public function clear() {
+  public function clear()
+  {
     $iterator = $this->getIterator();
     foreach ($iterator as $key => $item) {
       $iterator->offsetUnset($key);
@@ -83,7 +89,10 @@ abstract class GeneralAggregator extends ArrayObject {
    * @param \AmoCrm\Client\CustomField\FieldConfig|NULL $field_config
    * @return mixed
    */
-  abstract protected function createObject(array $data, FieldConfig $field_config = NULL);
+  abstract protected function createObject(
+    array $data,
+    FieldConfig $field_config = null
+  );
 
   /**
    * Get entity by ID
@@ -95,42 +104,63 @@ abstract class GeneralAggregator extends ArrayObject {
    * @throws \AmoCrm\Client\Exception\ResponseAggregatorException
    * @throws \AmoCrm\Client\Exception\UnknownEntityAggregatorException
    */
-  protected function _get($entities, $id) {
+  protected function _get($entities, $id)
+  {
     if (empty($id)) {
       throw new IdentifierAggregatorException('ID is empty');
     }
     if (!in_array($entities, ['contacts', 'leads', 'notes'])) {
-      throw new UnknownEntityAggregatorException(sprintf('Entities "%s" is not supported',
-        $entities));
+      throw new UnknownEntityAggregatorException(
+        sprintf(
+          'Entities "%s" is not supported',
+          $entities
+        )
+      );
     }
 
     // Search
-    if ($response_array = $this->request->setMethod(sprintf('/private/api/v2/json/%s/list',
-      $entities))
-      ->setQuery(NULL)
+    if ($response_array = $this->request->setMethod(
+      sprintf(
+        '/private/api/v2/json/%s/list',
+        $entities
+      )
+    )
+      ->setQuery(null)
       ->setId($id)
-      ->get()
-    ) {
+      ->get()) {
       if (isset($response_array['response'][$entities]) &&
-        is_array($response_array['response'][$entities])
-      ) {
-        $this->logger->debug(sprintf('Search in %s for ID "{id}" returned {results_count} result(s)',
-          $entities), [
-          'id'            => $id,
-          'results_count' => count($response_array['response'][$entities]),
-        ]);
+        is_array($response_array['response'][$entities])) {
+        $this->logger->debug(
+          sprintf(
+            'Search in %s for ID "{id}" returned {results_count} result(s)',
+            $entities
+          ),
+          [
+            'id' => $id,
+            'results_count' => count($response_array['response'][$entities]),
+          ]
+        );
 
         return $response_array['response'][$entities];
       } else {
-        throw new ResponseAggregatorException(sprintf('Bad %s response structure',
-          $entities));
+        throw new ResponseAggregatorException(
+          sprintf(
+            'Bad %s response structure',
+            $entities
+          )
+        );
       }
     }
 
-    $this->logger->debug(sprintf('Search in %s for ID "{id}" returned empty result',
-      $entities), ['id' => $id]);
+    $this->logger->debug(
+      sprintf(
+        'Search in %s for ID "{id}" returned empty result',
+        $entities
+      ),
+      ['id' => $id]
+    );
 
-    return NULL;
+    return null;
   }
 
   /**
@@ -145,54 +175,75 @@ abstract class GeneralAggregator extends ArrayObject {
    * @throws \AmoCrm\Client\Exception\ResponseAggregatorException
    * @throws \AmoCrm\Client\Exception\UnknownEntityAggregatorException
    */
-  protected function _load($entity_type, $query = NULL, $page_size = 50, callable $callback = NULL) {
+  protected function _load(
+    $entity_type,
+    $query = null,
+    $page_size = 50,
+    callable $callback = null
+  ) {
     if (!in_array($entity_type, ['contacts', 'leads'])) {
-      throw new UnknownEntityAggregatorException(sprintf('Entities "%s" is not supported',
-        $entity_type));
+      throw new UnknownEntityAggregatorException(
+        sprintf(
+          'Entities "%s" is not supported',
+          $entity_type
+        )
+      );
     }
 
     // Configure request
     $current_page = 0;
-    $this->request->setMethod(sprintf('/api/v2/%s',
-      $entity_type))
+    $this->request->setMethod(
+      sprintf(
+        '/api/v2/%s',
+        $entity_type
+      )
+    )
       ->setPageSize($page_size)
       ->setQuery($query)
-      ->setId(NULL);
+      ->setId(null);
 
     // Search
-    $batch = [];
+    $batch = $part = [];
     do {
-      $part = [];
       $response = $this->request->setPageNumber(++$current_page)
         ->get();
-      if (isset($response['response'][$entity_type]) &&
-        is_array($response['response'][$entity_type])
-      ) {
-        $part = $response['response'][$entity_type];
-        // Add log
-        $this->logger->debug(sprintf('Loaded {results_count} %s (page {current_page})',
-          $entity_type), [
-          'results_count' => count($part),
-          'query'         => $query,
-          'current_page'  => $current_page,
-        ]);
+      $part = $response['_embedded']['items'] ?? [];
+      if ($part && is_array($part)) {
+        $this->logger->debug(
+          sprintf(
+            'Loaded {results_count} %s (page {current_page})',
+            $entity_type
+          ),
+          [
+            'results_count' => count($part),
+            'query'         => $query,
+            'current_page'  => $current_page,
+          ]
+        );
 
         $batch = array_merge($batch, $part);
         if (!is_null($callback)) {
-          call_user_func_array($callback,
-            ['status' => 'loading', 'data' => $part]);
+          call_user_func_array(
+            $callback,
+            ['status' => 'loading', 'data' => $part]
+          );
         }
       }
-    } while (count($part) == $page_size);
+    } while ($part && count($part) == $page_size);
 
     // Add log
-    $this->logger->debug(sprintf('Total loaded {results_count} %s in {total_pages} requests',
-      $entity_type), [
-      'results_count' => count($batch),
-      'query'         => $query,
-      'total_pages'   => $current_page,
-    ]);
-    if (!is_null($callback)) {
+    $this->logger->debug(
+      sprintf(
+        'Total loaded {results_count} %s in {total_pages} requests',
+        $entity_type
+      ),
+      [
+        'results_count' => count($batch),
+        'query'         => $query,
+        'total_pages'   => $current_page,
+      ]
+    );
+    if (!is_null($callback) && is_callable($callback)) {
       call_user_func_array($callback, ['status' => 'loaded', 'data' => $batch]);
     }
 
@@ -210,14 +261,23 @@ abstract class GeneralAggregator extends ArrayObject {
    * @throws \AmoCrm\Client\Exception\UnknownEntityAggregatorException
    * @throws \AmoCrm\Client\Exception\UnknownOperationAggregatorException
    */
-  protected function _save($entities, $operation, $rows) {
+  protected function _save($entities, $operation, $rows)
+  {
     if (!in_array($entities, ['contacts', 'leads', 'notes'])) {
-      throw new UnknownEntityAggregatorException(sprintf('Entities "%s" is not supported',
-        $entities));
+      throw new UnknownEntityAggregatorException(
+        sprintf(
+          'Entities "%s" is not supported',
+          $entities
+        )
+      );
     }
     if (!in_array($operation, ['add', 'update'])) {
-      throw new UnknownOperationAggregatorException(sprintf('Operation "%s" is not supported',
-        $operation));
+      throw new UnknownOperationAggregatorException(
+        sprintf(
+          'Operation "%s" is not supported',
+          $operation
+        )
+      );
     }
 
     $request = [
@@ -228,30 +288,42 @@ abstract class GeneralAggregator extends ArrayObject {
       ],
     ];
 
-    if ($response_array = $this->request->setMethod(sprintf('/private/api/v2/json/%s/set',
-      $entities))
-      ->setQuery(NULL)
-      ->setId(NULL)
-      ->post($request)
-    ) {
+    if ($response_array = $this->request->setMethod(
+      sprintf(
+        '/private/api/v2/json/%s/set',
+        $entities
+      )
+    )
+      ->setQuery(null)
+      ->setId(null)
+      ->post($request)) {
       if (isset($response_array['response'][$entities][$operation]) &&
-        is_array($response_array['response'][$entities][$operation])
-      ) {
-        $this->logger->debug('Entities saved: operation={operation}, count={count}',
+        is_array($response_array['response'][$entities][$operation])) {
+        $this->logger->debug(
+          'Entities saved: operation={operation}, count={count}',
           [
             'operation' => $operation,
             'count'     => count($rows),
-          ]);
+          ]
+        );
 
         return $response_array['response'][$entities][$operation];
       } else {
-        throw new ResponseAggregatorException(sprintf('Bad %s response structure',
-          $entities));
+        throw new ResponseAggregatorException(
+          sprintf(
+            'Bad %s response structure',
+            $entities
+          )
+        );
       }
     }
 
-    throw new ResponseAggregatorException(sprintf('Bad %s response: empty or NULL',
-      $entities));
+    throw new ResponseAggregatorException(
+      sprintf(
+        'Bad %s response: empty or NULL',
+        $entities
+      )
+    );
   }
 
   /**
@@ -259,7 +331,8 @@ abstract class GeneralAggregator extends ArrayObject {
    *
    * @return \AmoCrm\Client\Request
    */
-  public function getRequest() {
+  public function getRequest()
+  {
     return $this->request;
   }
 
